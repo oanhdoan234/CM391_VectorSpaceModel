0a6101812d02437440e68ab4e1a216e4503a4d62
oanhdoan234 <doano@lafayette.edu>
2017-04-11 16:46:24 -0400

    Add news files

--- VectorSpaceModel2.py
+++ VectorSpaceModel2.py
@@ -189,7 +189,7 @@ def vectorize(X_train_raw):
     #   - Selects the 10,000 most frequently occuring words in the corpus.
     #   - Normalizes the vector (L2 norm of 1.0) to normalize the effect of
     #     document length on the tf-idf values.
-    vectorizer = TfidfVectorizer(max_df=0.5, max_features=10000,
+    vectorizer = TfidfVectorizer(max_df=0.5, max_features= None,
                              min_df=2, stop_words='english',
                              use_idf=True)
 
@@ -293,7 +293,7 @@ if __name__ == '__main__':
 	#   - Selects the 10,000 most frequently occuring words in the corpus.
 	#   - Normalizes the vector (L2 norm of 1.0) to normalize the effect of 
 	#     document length on the tf-idf values. 
-    vectorizer = TfidfVectorizer(max_df=0.5, max_features=10000,
+    vectorizer = TfidfVectorizer(max_df=0.5, max_features= 10000,
 								min_df=2, stop_words='english',
 								use_idf=True)
 
